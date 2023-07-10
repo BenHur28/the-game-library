c60eae2e0728d1e81a3a7d18aa34e5974bcc9c66
Ben Hur <Bhur777@gmail.com>
2023-07-09 21:28:10 -0400

    Add genre list spinner while fetching data

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,16 @@
 import useGenres from "../hooks/useGenres";
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-	const { data } = useGenres();
+	const { data, error, isLoading } = useGenres();
+
+	if (error) return null;
+	if (isLoading) return <Spinner />;
+
 	return (
 		<List>
+			<Text>{error}</Text>
 			{data.map((genre) => (
 				<ListItem key={genre.id} paddingY="5px">
 					<HStack>
