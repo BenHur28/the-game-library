27026ccd9f82480619c85254b3253a72d3efd6b0
Ben Hur <Bhur777@gmail.com>
2023-07-10 13:24:23 -0400

    Create platform select dropdown

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,7 +16,7 @@ const GameGrid = ({ selectedGenre }: Props) => {
 	return (
 		<>
 			{error && <Text>{error}</Text>}
-			<SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={5}>
+			<SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={5} paddingY={3}>
 				{isLoading &&
 					skeletons.map((skeleton) => (
 						<GameCardContainer key={skeleton}>
